e12c17a4320c6985cbfd51a5dd224a83587e1bfa
Ilja Krijger <ilja@quittheprogram.org>
2011-07-22 17:33:15 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timelords::Application.config.secret_token = '0d8e00726489a6fcca4e3d1835f8bbe08d79b8403952fe087815b935911c6660066c41800e542fb3b1435a9121f918ac75c17b54a572655fb6fa66bc33f6b010'
